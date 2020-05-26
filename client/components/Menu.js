bae99276f20ba42fe46f5034f80daddfc6671523
slayeroak <roakley1@villanova.edu>
2020-05-26 13:39:04 -0400

    Row

--- client/components/Menu.js
+++ client/components/Menu.js
@@ -4,10 +4,7 @@ import Router from 'next/router';
 import { isAuth, logout } from '../helpers/auth';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/row";
-import Column from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
+
 
 
 const Menu = () => (
